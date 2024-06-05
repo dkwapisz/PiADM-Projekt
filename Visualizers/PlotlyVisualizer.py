d8cb6244ddde2e802045a5be973651f0af722739
dkwapisz <dkwapisz99@gmail.com>
2024-06-05 18:29:26 +0200

    Refactor

--- Visualizers/PlotlyVisualizer.py
+++ Visualizers/PlotlyVisualizer.py
@@ -13,18 +13,8 @@ class PlotlyVisualizer:
         y = self.mesh.points[:, 1] * scale_factor
         z = self.mesh.points[:, 2] * scale_factor
 
-        fig = go.Figure(data=[
-            go.Mesh3d(
-                x=x,
-                y=y,
-                z=z,
-                i=self.mesh.triangles[:, 0],
-                j=self.mesh.triangles[:, 1],
-                k=self.mesh.triangles[:, 2],
-                color='lightpink',
-                opacity=0.50
-            )
-        ])
+        fig = go.Figure(data=[go.Mesh3d(x=x, y=y, z=z, i=self.mesh.triangles[:, 0], j=self.mesh.triangles[:, 1],
+                                        k=self.mesh.triangles[:, 2], color='lightpink', opacity=0.50)])
 
         max_range = np.array([x.ptp(), y.ptp(), z.ptp()]).max() * 0.5
 
@@ -35,21 +25,8 @@ class PlotlyVisualizer:
         y_eye = mid_y + max_range
         z_eye = mid_z + max_range
 
-        fig.update_layout(
-            width=1200,
-            height=800,
-            autosize=False,
-            scene=dict(
-                aspectratio=dict(
-                    x=np.ptp(x),
-                    y=np.ptp(y),
-                    z=np.ptp(z)
-                ),
-                aspectmode='manual',
-                camera=dict(
-                    eye=dict(x=x_eye, y=y_eye, z=z_eye)
-                )
-            )
-        )
+        fig.update_layout(width=1200, height=800, autosize=False,
+                          scene=dict(aspectratio=dict(x=np.ptp(x), y=np.ptp(y), z=np.ptp(z)), aspectmode='manual',
+                                     camera=dict(eye=dict(x=x_eye, y=y_eye, z=z_eye))))
 
         fig.show()
